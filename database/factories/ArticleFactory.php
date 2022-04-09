c0374818648aef3ab6f7ee2ca027b58e7e489ffb
jovialcore <jollydosky@gmail.com>
2022-04-09 13:50:45 +0100

    added the article seeders necessary

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Article::class;
+
+
+    public function definition()
+    {
+        return [
+            'title' =>  $this->faker->title,
+            'full_text' => $this->faker->text,
+            'cover' => $this->faker->imageUrl(),
+            'user_id' => $this->faker->numberBetween(1, 10),
+            'category_id' => $this->faker->numberBetween(1, 10),
+        ];
+    }
+}
